bd03b91ce07dd34d40bf32c681651bf05e89cca3
choiSUNJUNG <83733112+choiSUNJUNG@users.noreply.github.com>
2021-05-18 10:27:33 +0900

    Update autotrade_upbit_with15_1.py

--- autotrade_upbit_with15_1.py
+++ autotrade_upbit_with15_1.py
@@ -21,7 +21,7 @@ def get_start_time(ticker):
 def get_ma15(ticker):
     """15일 이동 평균선 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=25)
-    df['ma15'] = df['low'].rolling(15).mean()
+    df['ma15'] = df['close'].rolling(15).mean()
     # ma15 = df['close'].rolling(15).mean().iloc[-1]
     # return ma15
     return df
